0bc4ae5a41c1fd87e9ac2cb118ae560234792fbe
Carsten <49483082+Will-i-am-bot@users.noreply.github.com>
2025-10-24 11:03:24 +0200

    Update cronjob.php
    Update for a clean PHP 8.x Version

--- cronjob.php
+++ cronjob.php
@@ -1,54 +1,25 @@
 <?php
-
-/**
- *  2Moons 
- *   by Jan-Otto Kröpke 2009-2016
- *
- * For the full copyright and license information, please view the LICENSE
- *
- * @package 2Moons
- * @author Jan-Otto Kröpke <slaver7@gmail.com>
- * @copyright 2009 Lucky
- * @copyright 2016 Jan-Otto Kröpke <slaver7@gmail.com>
- * @licence MIT
- * @version 1.8.0
- * @link https://github.com/jkroepke/2Moons
- */
+// Log to see when the cronjob.php is triggered
+//file_put_contents(__DIR__.'/cron_trigger.log', date('Y-m-d H:i:s')."\n", FILE_APPEND);
 
 define('MODE', 'CRON');
-define('ROOT_PATH', str_replace('\\', '/',dirname(__FILE__)).'/');
+define('ROOT_PATH', str_replace('\\', '/', dirname(__FILE__)).'/');
 set_include_path(ROOT_PATH);
 
 require 'includes/common.php';
-
-$session	= Session::load();
-
-// Output transparent gif
-HTTP::sendHeader('Cache-Control', 'no-cache');
-HTTP::sendHeader('Content-Type', 'image/gif');
-HTTP::sendHeader('Expires', '0');
-
-
-echo("\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\x00\x00\x00\x00\x00\x00\x21\xF9\x04\x01\x00\x00\x00\x00\x2C\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02\x44\x01\x00\x3B");
-
-if(!$session->isValidSession())
-{
-	exit;
-}
-
-$cronjobID	= HTTP::_GP('cronjobID', 0);
-
-if(empty($cronjobID))
-{
-	exit;
-}
-
 require 'includes/classes/Cronjob.class.php';
 
-$cronjobsTodo	= Cronjob::getNeedTodoExecutedJobs();
-if(!in_array($cronjobID, $cronjobsTodo))
-{
-	exit;
+// Run all due cronjobs once per system call
+$cronjobsTodo = Cronjob::getNeedTodoExecutedJobs();
+
+if (empty($cronjobsTodo)) {
+    exit;
 }
 
-Cronjob::execute($cronjobID);
+foreach ($cronjobsTodo as $cronjobID) {
+    try {
+        Cronjob::execute($cronjobID);
+    } catch (Exception $e) {
+        file_put_contents(__DIR__.'/cron_error.log', '['.date('Y-m-d H:i:s')."] ".$e->getMessage()."\n", FILE_APPEND);
+    }
+}
